2f0375e4697061b7745c886db889bb4cf63923f4
livemoon <mwjpiero@gmail.com>
2011-11-29 11:37:33 +0800

    Add echo in  restart_nova-compute.sh

--- scripts/restart_nova-compute.sh
+++ scripts/restart_nova-compute.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-
+echo "Killing nova-compute ..."
 ps aux | awk '/[n]ova-compute$/ {print $2}'| xargs echo
+echo "Restarting libvirt-bin ..."
 sudo /etc/init.d/libvirt-bin restart
 if [ "$?" -eq 0 ]; then
-    sleep 10
-    cd $HOME/nova && sudo sg libvirtd bin/nova-compute &
+    sleep 5
+    echo "Starting nova-compute ..."
+    cd $HOME/nova && (sudo sg libvirtd bin/nova-compute &)
 fi
