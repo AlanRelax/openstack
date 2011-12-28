e82108e932923578f1859bb51e7d76da0450c192
livemoon <mwjpiero@gmail.com>
2011-12-28 15:49:00 +0800

    add if nova-compute exists

--- scripts/restart_nova-compute.sh
+++ scripts/restart_nova-compute.sh
@@ -5,10 +5,13 @@ for i in `ps aux | awk '/[n]ova-compute$/ {print $2}'`
 do
     sudo kill $i
 done
-echo "Restarting libvirt-bin ..."
-sudo /etc/init.d/libvirt-bin restart
-if [ "$?" -eq 0 ]; then
-    sleep 5
-    echo "Starting nova-compute ..."
-    cd $HOME/nova && (sudo su -c "sg libvirtd bin/nova-compute > /dev/null 2>&1 &" stack)
+ps aux | grep [n]ova-compute > /dev/null
+if [ $? -eq 1 ];then
+    echo "Restarting libvirt-bin ..."
+    sudo /etc/init.d/libvirt-bin restart
+    if [ "$?" -eq 0 ]; then
+        sleep 5
+        echo "Starting nova-compute ..."
+        cd $HOME/nova && (sudo su -c "sg libvirtd bin/nova-compute > /dev/null 2>&1 &" stack)
+    fi
 fi
