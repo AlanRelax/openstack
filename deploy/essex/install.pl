249cbb08456b1348e4e4c364e22be26244a335d5
livemoon <deadsun@desktop.my.domain>
2012-12-18 17:07:41 +0800

    Add ntp installation in deploy/essex/install.pl

--- deploy/essex/install.pl
+++ deploy/essex/install.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 
 use strict;
-use 5.010;
+use 5.010001;
 use Config::IniFiles;
 
 
@@ -22,11 +22,20 @@ sub install_common {
     system "apt-get", "update";
     system "apt-get", "-y", "upgrade";
     system "apt-get", "-y", "dist-upgrade";
-    system "apt-get", "-y", "ntp" if ( system "dpkg", "-l", "ntp");
-    open my $fh, "+<", "/etc/ntp.conf"
-	or die "Can't open /etc/ntp.conf: $!";
-    while ( <$fh> ) {
-}
+    system "apt-get", "-y", "install", "ntp" if ( system "dpkg", "-l", "ntp");
+    @ARGV = qw ! /etc/ntp.conf !;
+    $^I = ".bak";
+    my $flag;
+    while( <> ) {
+	given( $_ ) {
+	    when ( s/\A(server ntp.ubuntu.com)\Z/$1 iburst/ ) { print; }
+	    when ( /\Aserver 127.127.1.0\Z/ ) { $flag += 1; print; }
+	    when ( /\Afudeg\s+127\.127\.1\.0\s+stratum\s+10\Z/) { $flag += 2; print }
+	    default { print; }
+	}
+    }
+    print $flag;
+}   
 
 &install_common;
 
