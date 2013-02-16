141767d7d9a15439cc80a2056fca571739f27e23
livemoon <deadsun@laptop.202.96.209.5>
2013-02-16 22:07:38 +0800

    Add new files

--- deploy/essex/install.pl
+++ deploy/essex/install.pl
@@ -3,6 +3,7 @@
 use strict;
 use 5.010001;
 use Config::IniFiles;
+use DBI;
 
 
 #sub parse {
@@ -76,9 +77,14 @@ sub install_mysql {
     my $dbh = DBI->connect("DBI:mysql:mysql:$host:3306", "root", $root_pass, { RaiseError => 1, AutoCommit => 1 });
     foreach (&get_ini($ini_file, "MYSQL", "db")) {
 	$dbh->do(qq{CREATE DATABASE $_});
-	my $dbadmin = $_."dbadmin";
-	$dbh->do(qq{GRANT ALL PRIVILEGES ON $_.* TO "$dbadmin"@'%' IDENTIFIED BY "$db_pass"});
+	my $db_admin = $_."dbadmin";
+#	$dbh->do(qq{GRANT ALL PRIVILEGES ON $_.* TO "$dbadmin"@'%' IDENTIFIED BY "$db_pass"});
+	my $query = qq{GRANT ALL PRIVILEGES ON $_.* TO ?\@'%' IDENTIFIED BY ?};
+	dbh->do($query, undef, $db_admin, $db_pass);
+	my $query = qq{GRANT ALL PRIVILEGES ON $_.* TO ?\@'localhost' IDENTIFIED BY ?};
+	dbh->do($query, undef, $db_admin, $db_pass);
     }
+    $dbh->disconnect();
     print $root_pass;
 }
 
@@ -90,16 +96,58 @@ sub install_rabbitmq {
 
 sub install_keystone {
     system "apt-get", "install", "-y", "keystone", "python-keystone", "python-keystoneclient" if &exist_app("keystone");
-    &config_app("./config/keystone.conf", "/etc/keystone/keystone.conf");
+    &config_app("./config/keystone/keystone.conf", "/etc/keystone/keystone.conf");
     system "service", "keystone", "restart";
     system "keystone-manage", "db_sync";
     my $token = (&get_ini("./config/keystone.conf", "DEFAULT", "admin_token"))[0];
     my $endpoint = (&get_ini($ini_file, "KEYSTONE", "endpoint"))[0];
     $ENV{SERVICE_TOKEN} = $token;
     $ENV{SERVICE_ENDPOINT} = $endpoint;
+    my $db_pass = (&get_ini($ini_file, "MYSQL", "db_password"))[0];
+    my $region = (&get_ini($ini_file, "KEYSTONE", "region"))[0];
+    my $dbh = DBI->connect("DBI:mysql:keystone:$host:3306", "keystonedbadmin", $db_pass, { RaiseError => 1, AutoCommit => 1});
     foreach (&get_ini($ini_file, "KEYSTONE", "service")) {
 	my @service = &get_ini($ini_file, "KEYSTONE_SERVICE", $_);
 	system "keystone", "service-create", "--name", $_ , "--type", $service[0], "--description", $service[1], "\n";
+	my $sth = $dbh->prepare('SELECT ID FROM service WHERE type=?');
+	$sth->bind_param(1, $service[0]);
+	$sth->execute();
+	my $result = $sth->fetchrow_hashref();
+	my @endpoint = &get_ini($ini_file, "KEYSTONE_ENDPOINT", $service[0]);
+	system "keystone", "endpoint-create", "--region", $endpoint[0], "--service_id", $result->{id}, "--publicurl", $endpoint[1], "--adminurl", $endpoint[2], "--internalurl", $endpoint[3];
+    }
+    $dbh->disconnect();
+}
+
+sub install_glance {
+    system "apt-get", "install", "-y", "glance", "glance-api", "glance-client", "glance-common", "glance-registry", "python-glance" if &exit_app("glance");
+    &config_app("./config/glance/glance-api-paste.ini", "/etc/glance/glance-api-paste.ini");
+    &config_app("./config/glance/glance-registry-paste.ini", "/etc/glance/glance-registry-paste.ini");
+    &config_app("./config/glance/glance-api.conf", "/etc/glance/glance-api.conf");
+    &config_app("./config/glance/glance-registry.conf", "/etc/glance/glance-registry.conf");
+    system "glance-manage", "version_control", "0";
+    system "glance-manage", "db_sync";
+    system "service", "glance-api", "restart";
+    system "service", "glance-registry", "restart";
+    system "wget";
+}
+
+sub install_nova {
+#my @nova_pkg = qq/apt-get install -y nova-api nova-cert nova-common/;
+#system(@nova_pkg);
+#&config_app("./config/nova/nova.conf", "/etc/nova/nova.conf");
+    my @nova_service = &get_ini($ini_file, "NOVA", "service");
+    foreach (@nova_service) {
+	print "service", $_, "stop";
+    }
+    sleep(10);
+    foreach (@nova_service) {
+	print "service", $_, "start";
+    }
+    sleep(10);
+    print "nova-manage", "db", "sync";
+    foreach (@nova_service) {
+	print "service", $_, "restart";
     }
 }
 
@@ -111,4 +159,3 @@ foreach (&get_ini($ini_file, "KEYSTONE", "service")) {
     my @service = &get_ini($ini_file, "KEYSTONE_SERVICE", $_);
     print "keystone", "service-create", "--name", $_ , "--type", $service[0], "--description", $service[1], "\n";
 }
-
